cf214e4453f5681ef22d6afbb1d650b1de2f8d1b
mysh <mysh995@gmail.com>
2020-01-16 15:45:34 +0000

    [1-3] comparing matrices with NaNs

--- 1-3/voting.py
+++ 1-3/voting.py
@@ -26,6 +26,10 @@ def vote_borda(data: pd.DataFrame):
     return data.sum()
 
 
+def is_equal_or_null(d1, d2):
+    return (d1.eq(d2) + d1.isnull() + d2.isnull()).all().all()
+
+
 def vote_runoff(data: pd.DataFrame, n_rounds=2, verbose=False):
     if n_rounds < 1:
         raise ValueError("Number of rounds must be a positive integer")
@@ -45,7 +49,7 @@ def vote_runoff(data: pd.DataFrame, n_rounds=2, verbose=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-fname', type=str, default=r"..\1-3\countries.xlsx")
-    parser.add_argument('-sheet', default=1)
+    parser.add_argument('-sheet', default=0)
 
     parsed_args = parser.parse_args()
     d = import_data(parsed_args.fname, parsed_args.sheet)
@@ -53,4 +57,9 @@ if __name__ == '__main__':
     print(f"\nBorda count vote:\n{vote_borda(d)}")
     print(f"\nRunoff vote (2 rounds):\n{vote_runoff(d)}")
     print(f"\nRunoff vote (3 rounds):\n{vote_runoff(d, 3)}")
+
+    print(f"\n{20*'-'}\nComparing matrices")
+    dd = d[:2]
+    dd2 = dd.fillna(0)
+    print(dd, '\n', dd2, '\n', is_equal_or_null(dd, dd2))
 
