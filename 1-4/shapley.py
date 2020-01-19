3325e717c475fb4d404a6e8f678fc8544a8e93ea
mysh <mysh995@gmail.com>
2020-01-19 21:30:52 +0000

    [1-4] shapley values

--- 1-4/shapley.py
+++ 1-4/shapley.py
@@ -1,7 +1,7 @@
 """Shapley value calculator."""
 
 import itertools as it
-
+from math import factorial
 
 def validate(message, t=float):
     val = 0
@@ -24,12 +24,29 @@ def validate(message, t=float):
 if __name__ == '__main__':
     N = validate("Enter number of agents: ", int)
 
+    FN = factorial(N)
+
     ag = range(1, N+1)
 
-    v = dict()
+    v = {(): 0}
 
     for i in ag:
         for co in it.combinations(ag, i):
-            print(co)
+            v[co] = validate(f"Enter value for coalition {co}: ")
 
+    print(v)
+
+    shap = dict()
+    for i in ag:
+        shap[i] = 0
+
+        for co in v.keys():
+            if i not in co:
+                continue
+
+            shap[i] += v[co] - v[tuple(set(co) - {i})]
+
+        shap[i] /= FN
+
+    print(f"\nShapley values: {shap}")
 
