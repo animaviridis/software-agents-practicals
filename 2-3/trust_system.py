83261f2cee39b82427e16d1ef1b4452927ec031e
mysh <mysh995@gmail.com>
2020-01-26 19:59:52 +0000

    [2-3] Code reformating

--- 2-3/trust_system.py
+++ 2-3/trust_system.py
@@ -2,74 +2,78 @@ import random
 import networkx as nx
 import time
 
+
 class Agent:
-  def __init__(self,competence):
-    self.competence=competence
-    self.neighbours=set()
-    self.score=[0,0]
+    def __init__(self, competence):
+        self.competence = competence
+        self.neighbours = set()
+        self.score = [0, 0]
 
-  def do_task(self):
-    return random.random()<self.competence 
+    def do_task(self):
+        return random.random() < self.competence
 
-  def delegate(self):
-    success=False
-    if self.pick_partner().do_task():
-            self.score[0]+=1
-            success=True
-    else:
-            self.score[1]+=1          
-    return success        
-  
-  def get_reputation(self,target):
-    """returns the reputation value for agent target according to this agent"""
-    pass
-    
-  def pick_partner(self):
-    """select a partner for interaction from the agent's neighbours"""
-    pass            
+    def delegate(self):
+        success = False
+        if self.pick_partner().do_task():
+            self.score[0] += 1
+            success = True
+        else:
+            self.score[1] += 1
+        return success
 
+    def get_reputation(self, target):
+        """returns the reputation value for agent target according to this agent"""
+        pass
+
+    def pick_partner(self):
+        """select a partner for interaction from the agent's neighbours"""
+        pass
 
 
 class Environment(nx.DiGraph):
-  def add_agents(self,agents):
-    m={}
-    i=0
-    for a in agents:
-            m[i]=a
-            i+=1
-    nx.relabel_nodes(self,m,copy=False)
-    
-    for n in self.nodes:
-            n.neighbours=set(nx.neighbors(self,n))
+    def add_agents(self, agents):
+        m = {}
+        i = 0
+        for a in agents:
+            m[i] = a
+            i += 1
+        nx.relabel_nodes(self, m, copy=False)
 
-  def tick(self):
-    score=[0,0]
-    for n in self.nodes:
+        for n in self.nodes:
+            n.neighbours = set(nx.neighbors(self, n))
+
+    def tick(self):
+        score = [0, 0]
+        for n in self.nodes:
             if self.delegate():
-                    score[0]+=1
+                score[0] += 1
             else:
-                    score[1]+=1        
-    return score                
+                score[1] += 1
+        return score
 
-########################RUN THE EXPERIMENT###########################
-NUMAGENTS=10 #Number of agents 
+#    ########################RUN THE EXPERIMENT###########################
 
-random.seed(0) #set the random seed
-a=[]
-for i in range(0,NUMAGENTS):
-        a.append(Agent(random.random()))  
 
-G=nx.complete_graph(NUMAGENTS) #create a complete graph, see https://networkx.github.io/documentation/stable/reference/generators.html for other generators
-E=Environment(G)
-E.add_agents(a) 
-#random.seed(time.time()) #uncomment if you want different experiments on same graph
-for i in range(0,100): #run for 100 rounds
-        score=[0,0]
-        for a in E.nodes:
-                s=a.delegate()
-                if s:
-                  score[0]+=1
-                else:  
-                  score[1]+=1
-        print(score)        
+NUMAGENTS = 10  # Number of agents
 
+random.seed(0)  # set the random seed
+a = []
+for i in range(0, NUMAGENTS):
+    a.append(Agent(random.random()))
+
+# create a complete graph,
+# see https://networkx.github.io/documentation/stable/reference/generators.html for other generators
+G = nx.complete_graph(NUMAGENTS)
+E = Environment(G)
+E.add_agents(a)
+
+# random.seed(time.time()) #uncomment if you want different experiments on same graph
+for i in range(0, 100):  # run for 100 rounds
+    score = [0, 0]
+    for a in E.nodes:
+        s = a.delegate()
+        if s:
+            score[0] += 1
+        else:
+            score[1] += 1
+    print(score)
