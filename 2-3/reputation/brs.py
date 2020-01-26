e2f5b2a3c3cd63fa42dbca0e1db428c5843ea305
mysh <mysh995@gmail.com>
2020-01-26 21:33:37 +0000

    [2-3] BRS: discounting beliefs

--- 2-3/reputation/brs.py
+++ 2-3/reputation/brs.py
@@ -21,9 +21,35 @@ def brs_rating(a, b):
     return (a - b) / (a + b + 2)
 
 
+@smoothing
+def brs_opinion(r, s):
+    den = r + s + 2  # denominator
+
+    b = r / den  # belief?
+    d = s / den  # ?
+    u = 2 / den  # uncertainty?
+
+    return b, d, u
+
+
+def discount_belief(xy, yz):
+    b_xy, d_xy, u_xy = xy
+    b_yz, d_yz, u_yz = yz
+
+    b = b_xy * b_yz
+    d = b_xy * d_yz
+    u = d_xy + u_xy + b_xy * u_yz
+
+    return b, d, u
+
+
 if __name__ == '__main__':
     print(brs_expected(10, 10))
     print(brs_rating(10, 10), '\n')
 
     print(brs_rating(20, 10))
     print(brs_rating(20, 10, False))
+
+    print("\nb, d, u: ", brs_opinion(10, 20))
+
+    print('\nDiscounting beliefs:', discount_belief(brs_opinion(10, 20), brs_opinion(30, 2)))
