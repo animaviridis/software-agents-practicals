806906ffaaa75bfcccaf7c182aacde1ff45ba2b7
mysh <mysh995@gmail.com>
2020-01-27 15:03:38 +0000

    [2-3] BRS: decoding and addition

--- 2-3/reputation/brs.py
+++ 2-3/reputation/brs.py
@@ -54,8 +54,25 @@ class BRSOpinion(tuple):
         d = b_xy * d_yz
         u = d_xy + u_xy + b_xy * u_yz
 
-        return b, d, u
+        return BRSOpinion(*BRSOpinion.decode_bdu(b, d, u))
 
+    def __add__(self, other):
+        if not isinstance(other, BRSOpinion):
+            raise TypeError("Not a BRSOpinion object")
+
+        r = self.r + other.r
+        s = self.s + other.s
+        return BRSOpinion(r, s)
+
+    def expected(self, *args, **kwargs):
+        return brs_expected(self.r, self.s, *args, **kwargs)
+
+    @staticmethod
+    def decode_bdu(b, d, u):
+        return 2*b/u, 2*d/u
+
+    def decode(self):
+        return self.decode_bdu(*self)
 
 # TODO: forgetting
 
@@ -67,9 +84,23 @@ if __name__ == '__main__':
     print(brs_rating(20, 10))
     print(brs_rating(20, 10, False))
 
-    brs_xy = BRSOpinion(5, 1)
-    brs_yz = BRSOpinion(4, 1)
+    brs_ab = BRSOpinion(5, 2)
+    brs_ac = BRSOpinion(1, 0)
+    brs_ad = BRSOpinion(4, 3)
 
-    print("XY b, d, u: ", brs_xy)
-    print("YZ b, d, u: ", brs_yz)
-    print("XZ b, d, u: ", brs_xy * brs_yz)
+    brs_cb = BRSOpinion(6, 2)
+    brs_db = BRSOpinion(1, 0)
+
+    print("AB b, d, u: ", brs_ab)
+    print("AC b, d, u: ", brs_ac)
+    print("AD b, d, u: ", brs_ad)
+
+    print()
+    print("ACB b, d, u: ", brs_ac * brs_cb)
+    print("ADB b, d, u: ", brs_ad * brs_db)
+
+    brs_acb = brs_ac * brs_cb
+    brs_adb = brs_ad * brs_db
+    brs_ = brs_acb + brs_adb
+
+    print(f"\nExpected value: A of B based on C and D: {brs_.expected(False)}")
