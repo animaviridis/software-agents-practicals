8465ab2d9fef86272831d2259aaf6271f6165429
mysh <mysh995@gmail.com>
2020-01-29 23:12:45 +0000

    [Assessment][1a] ver2: attack matrix

---
+++ assessment/task1a_v2.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+import pandas as pd
+import copy
+
+from parser_abstract import read_file
+
+arguments, rules = read_file(sys.argv[1] if len(sys.argv) > 1 else 'aaf.aaf')  # TODO: parser
+la = len(arguments)
+arguments_sorted = sorted(arguments)
+
+attacks = pd.DataFrame(np.zeros((la, la), int), index=arguments_sorted, columns=arguments_sorted)
+
+for rule in rules:
+    attacks.loc[rule[0], rule[1]] = 1
+
+
+print(f"\nAttack matrix:\n{attacks}")
+
+
