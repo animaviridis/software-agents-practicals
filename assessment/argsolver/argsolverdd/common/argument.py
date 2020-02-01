0df9aa5192791625b00238f80b5c5dfc417b0c87
mysh <mysh995@gmail.com>
2020-02-01 15:22:38 +0000

    [Assessment][2a] Refactoring contd.

--- assessment/argsolver/argsolverdd/common/argument.py
+++ assessment/argsolver/argsolverdd/common/argument.py
@@ -1,23 +1,26 @@
 from argsolverdd.common.rule import *
+
+
 class Argument:
-  def __init__(self,toprule,subarguments):
-    self.toprule=toprule
-    self.subarguments=subarguments
+    def __init__(self, toprule, subarguments):
+        self.toprule = toprule
+        self.subarguments = subarguments
 
-  def __hash__(self):
-    s=0
-    for sa in self.subarguments:
-            s+=hash(sa)
-    return hash((self.toprule,s))          
+    def __hash__(self):
+        s = 0
+        for sa in self.subarguments:
+            s += hash(sa)
+        return hash((self.toprule, s))
 
-  def __eq__(self,other):
-    if type(self)!=type(other): return False
-    if self.toprule!=other.toprule: return False
-    for s in self.subarguments:
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return False
+        if self.toprule != other.toprule:
+            return False
+        for s in self.subarguments:
             if s not in other.subarguments:
-                    return False
-    return True                
+                return False
+        return True
 
-  def strict(self):
-    pass
-                      
+    def strict(self):
+        pass
