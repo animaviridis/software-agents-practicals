2886fc5287d61c40115cdf887e82884b81a5dec5
mysh <mysh995@gmail.com>
2020-02-01 20:58:19 +0000

    [Assessment][2b] Counting attacks by rebuttal

--- assessment/task2/task2b.py
+++ assessment/task2/task2b.py
@@ -1,9 +1,21 @@
-import parser
 import sys
 
-(rules,preferences)=parser.read_file(sys.argv[1])
+from argsolverdd.common.parser import read_file
+from argsolverdd.common.argument import Argument
 
-arguments=make_arguments(rules)
-attacks=make_attacks(arguments)
+
+rules, preferences = read_file(sys.argv[1])
+
+arguments = Argument.make_arguments(rules)
+
+attacks = set()
+for a1 in arguments:
+    for a2 in arguments:
+        if a1 == a2:
+            continue
+
+        if a1.rebuts(a2):
+            attacks.add((a1, a2))
+
 print(len(attacks))
 
