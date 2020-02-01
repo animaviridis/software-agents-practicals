ce30a66cf1ad8b610776f02b00ae28f3bdc98f92
mysh <mysh995@gmail.com>
2020-02-01 15:03:03 +0000

    [Assessment][2a] Refactoring, minor changes

--- assessment/argsolver/argsolverdd/common/rule.py
+++ assessment/argsolver/argsolverdd/common/rule.py
@@ -1,31 +1,30 @@
 class Rule:
-  def __init__(self,name,prems,conc,strict):
-    self.name=name
-    self.prems=prems
-    self.conc=conc
-    self.strict=strict
+    def __init__(self, name, premises, conc, strict):
+        self.name = name
+        self.premises = premises
+        self.conclusions = conc
+        self.strict = strict
 
-  def __repr__(self):
-    s="=>"
-    if self.strict:
-            s="->"
-    p=""
-    for pr in self.prems:
-            p+=", "+str(pr)
-    p=p[1:]        
-    return ""+self.name+":"+p+" "+s+" "+str(self.conc)  
+    def __repr__(self):
+        s = "=>"
+        if self.strict:
+            s = "->"
+        p = ""
+        for pr in self.premises:
+            p += ", " + str(pr)
+        p = p[1:]
+        return "" + self.name + ":" + p + " " + s + " " + str(self.conclusions)
 
-  def __hash__(self):
-    s=0
-    if len(self.prems)>0:
-      for p in self.prems:
-            s+=hash(p)
-    return hash((s,self.name,self.conc,self.strict))
+    def __hash__(self):
+        s = 0
+        if len(self.premises) > 0:
+            for p in self.premises:
+                s += hash(p)
+        return hash((s, self.name, self.conclusions, self.strict))
 
-  def __eq__(self,other):
-    if type(self)!=type(other):
+    def __eq__(self, other):
+        if type(self) != type(other):
             return False
-    if self.prems-other.prems!=set() or other.prems-self.prems!=set():
+        if self.premises - other.premises != set() or other.premises - self.premises != set():
             return False
-    return self.name==other.name and self.conc == other.conc and self.strict==other.strict        
-
+        return self.name == other.name and self.conclusions == other.conc and self.strict == other.strict
--- assessment/task2/task2a.py
+++ assessment/task2/task2a.py
@@ -1,16 +1,19 @@
-import parser
+"""Given a structured argumentation framework as input, your task here is to print out the number of strict
+and defeasible arguments which can be generated. You should base your answer on the file task2a.py,
+modifying it as needed."""
+
 import sys
+from argsolverdd.common.parser import read_file
 
-(rules,preferences)=parser.read_file(sys.argv[1])
+(rules, preferences) = read_file(sys.argv[1])
 
-arguments=make_arguments(rules)
-strict=0
-defeasible=0
+arguments = make_arguments(rules)
+strict = 0
+defeasible = 0
 for a in arguments:
-        if a.strict():
-                strict+=1
-        else:
-                defeasible+=1
+    if a.strict():
+        strict += 1
+    else:
+        defeasible += 1
 
-print(strict,defeasible)
-
+print(strict, defeasible)
