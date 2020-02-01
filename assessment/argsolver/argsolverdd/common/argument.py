6dac208c62fce1658fe324cbf3d0f23ab3c0b8c6
mysh <mysh995@gmail.com>
2020-02-01 18:37:13 +0000

    [Assessment][2a] Sub-arguments

--- assessment/argsolver/argsolverdd/common/argument.py
+++ assessment/argsolver/argsolverdd/common/argument.py
@@ -5,7 +5,19 @@ class Argument:
     def __init__(self, name: str, top_rule: Rule):
         self.name = name
         self._top_rule = top_rule
-        self.sub_arguments = set()
+        self._sub_arguments = set()
+
+    @property
+    def direct_sub_arguments(self):
+        return self._sub_arguments
+
+    @property
+    def all_sub_arguments(self):
+        all_subs = set(self.direct_sub_arguments)
+        for sub in self.direct_sub_arguments:
+            for subsub in sub.all_sub_arguments:
+                all_subs.add(subsub)
+        return all_subs
 
     @property
     def top_rule(self):
@@ -18,7 +30,7 @@ class Argument:
     @property
     def ground_premises(self):
         prem = set(self.premises)
-        for arg in self.sub_arguments:
+        for arg in self._sub_arguments:
             prem.add(arg)
             prem -= {arg.conclusions}
 
@@ -35,11 +47,11 @@ class Argument:
     def _add_sub_argument(self, arg):
         if not isinstance(arg, type(self)):
             raise TypeError(f"Argument is not an instance of {type(self)}")
-        self.sub_arguments.add(arg)
+        self._sub_arguments.add(arg)
 
     def __hash__(self):
         s = 0
-        for sa in self.sub_arguments:
+        for sa in self._sub_arguments:
             s += hash(sa)
         return hash((self.top_rule, s))
 
@@ -48,8 +60,8 @@ class Argument:
             return False
         if self.top_rule != other.top_rule:
             return False
-        for s in self.sub_arguments:
-            if s not in other.sub_arguments:
+        for s in self.all_sub_arguments:
+            if s not in other.all_sub_arguments:
                 return False
         return True
 
